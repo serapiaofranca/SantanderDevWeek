31e6790d7c42492b4bffb7a89eb8fb2772c7f45e
Rogerio A Serapiao <rserapa@gmail.com>
2021-06-06 17:10:49 -0300

    Testando acoes no main_menu - log
    Tarefa: https://trello.com/c/bJ9WL48q/7-monitorar-o-item-do-mainmenu

--- app/src/main/java/com/rogerio/santanderdevweek/MainActivity.kt
+++ app/src/main/java/com/rogerio/santanderdevweek/MainActivity.kt
@@ -2,7 +2,9 @@ package com.rogerio.santanderdevweek
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +18,24 @@ class MainActivity : AppCompatActivity() {
         val inflate = menuInflater
         inflate.inflate(R.menu.main_menu, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_1 -> {
+                Log.d( "CLICK", "Clicou no item 1")
+                true
+            }
+            R.id.item_2 -> {
+                Log.d( "CLICK", "Item 2 selecionado com sucesso!!")
+                true
+            }
+             R.id.item_3 -> {
+                Log.d( "CLICK", " Acionado pelo ícone!!")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
 }
