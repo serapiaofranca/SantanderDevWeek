a9c60cbe060fc790c98afb1bda91422851634ddf
Rogerio A Serapiao <rserapa@gmail.com>
2021-06-07 16:54:32 -0300

    Adicionado Card para exibir detalhes de informações
    Tarefa: https://trello.com/c/mKeX7sEl/10-adicionar-card-para-exibir-informacoes

--- app/src/main/java/com/rogerio/santanderdevweek/MainActivity.kt
+++ app/src/main/java/com/rogerio/santanderdevweek/MainActivity.kt
@@ -13,29 +13,4 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
     }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val inflate = menuInflater
-        inflate.inflate(R.menu.main_menu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.item_1 -> {
-                Log.d( "CLICK", "Clicou no item 1")
-                true
-            }
-            R.id.item_2 -> {
-                Log.d( "CLICK", "Item 2 selecionado com sucesso!!")
-                true
-            }
-             R.id.item_3 -> {
-                Log.d( "CLICK", " Acionado pelo ícone!!")
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
 }
